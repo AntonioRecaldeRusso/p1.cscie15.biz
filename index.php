132c8fb14cb1bd6d61421140e0e3020425386010
asmallorange <jeanalesi007@gmail.com>
2013-09-14 02:21:06 -0400

    Adding index.php file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+	<head>
+		<title>CSCI E-15 Project 1</title>
+		<style>
+			body, html	{background-image: url(http://images2.layoutsparks.com/1/205492/jess-addiction-plain-simple.jpg); height: 100%;}
+			#wrapper{width: 60%; margin-left: auto; margin-right: auto; background-color: WHITE; height: 99%}
+			h1		{text-align: center;}
+
+			.about 	{font-size: 18px;}
+
+
+		</style>
+	</head>
+	<body>
+	<div id="wrapper">
+		<h1>CSCI E-15 Project 1</h1>
+		<p>
+			<div><h2>Name: Antonio Recalde</h2></div>
+			<div class="about">	<strong>About:</strong></br>
+					Hello there. I'm delighted to be part of this course. This is the fourth course I'm taking with with Harvard's Extension Program.</br>
+					I have experience with C, C++, Java, Python. But, I would say I'm new to Web Development.</br>
+					Some of my hobbies are: Tennis, Chess, Philosophy (I have actually produced a very respectable theory about the origin of morality. Still unpublished), Soccer, Video Games.
+			</div>
+		</p>
+		<p>
+			I'm using <strong>Sublime Text 2</strong>.
+		</p>
+
+		</table>
+	</div>
+	</body>
+</html>
